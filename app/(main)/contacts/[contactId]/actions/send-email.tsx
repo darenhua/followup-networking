30a0be84b8f69fd47ba5b8fd6b8f4a93a2ef06a6
Daren Hua <59425042+darenhua@users.noreply.github.com>
2024-08-03 15:30:47 -0400

    comment broken file for deployment

--- app/(main)/contacts/[contactId]/actions/send-email.tsx
+++ app/(main)/contacts/[contactId]/actions/send-email.tsx
@@ -1,31 +1,31 @@
-'use server'
+// 'use server'
 
-import { AuthenticatedHttpClient } from '@/lib/axios'
-import { createSafeActionClient } from 'next-safe-action'
-import { revalidatePath } from 'next/cache'
-import { z } from 'zod'
+// import { AuthenticatedHttpClient } from '@/lib/axios'
+// import { createSafeActionClient } from 'next-safe-action'
+// import { revalidatePath } from 'next/cache'
+// import { z } from 'zod'
 
-const schema = z.object({
-    app_password: z.string().min(1),
-    from: z.string().email(),
-    subject: z.string().min(1),
-    body: z.string().min(1),
-    to: z.string().email(),
-})
+// const schema = z.object({
+//     app_password: z.string().min(1),
+//     from: z.string().email(),
+//     subject: z.string().min(1),
+//     body: z.string().min(1),
+//     to: z.string().email(),
+// })
 
-const actionClient = createSafeActionClient()
+// const actionClient = createSafeActionClient()
 
-export const launchCampaignAction = actionClient
-    .schema(schema)
-    .action(async ({ parsedInput: { app_password, from, subject, body, to } }) => {
-        const httpClient = await AuthenticatedHttpClient()
-        const res = await httpClient.post('/launch_campaign/', {
-            campaign_name,
-        })
-        if (res.status === 400) {
-            throw new Error('Invalid campaign')
-        }
+// export const launchCampaignAction = actionClient
+//     .schema(schema)
+//     .action(async ({ parsedInput: { app_password, from, subject, body, to } }) => {
+//         const httpClient = await AuthenticatedHttpClient()
+//         const res = await httpClient.post('/launch_campaign/', {
+//             campaign_name,
+//         })
+//         if (res.status === 400) {
+//             throw new Error('Invalid campaign')
+//         }
 
-        revalidatePath(`/campaigns`)
-        revalidatePath(`/campaigns/${campaign_name}`)
-    })
+//         revalidatePath(`/campaigns`)
+//         revalidatePath(`/campaigns/${campaign_name}`)
+//     })
